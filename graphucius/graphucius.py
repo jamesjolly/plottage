884de897046e93680835c898ab285c14b0a4ffd2
James Jolly <jamesjolly@gmail.com>
2013-04-26 18:46:42 -0700

    changed graph tool name, added searches to base class

---
+++ graphucius/graphucius.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+"""
+plottage 0.1
+Copyright (C) 2013, James Jolly
+See MIT-LICENSE.txt for legalese and README.md for usage.
+"""
+
+from collections import defaultdict
+
+class graph:
+
+	def __init__(self):
+		self.graph = defaultdict(list)
+
+	def add_edge(self, node_from, node_to, weight=0):
+		self.graph[node_from].append((node_to, weight))
+
+	def __iter__(self):
+		return self.iter_edges()
+
+	def iter_edges(self):
+		for node_from, edges in self.graph.items():
+			for node_to, weight in edges:
+				yield (node_from, node_to, weight)
+
+	def search(self, start_node, search_type="bfs"):
+		q = [start_node]
+		visited = set()
+		while len(q) > 0:
+			if search_type == "dfs": node_id = q.pop()
+			else: node_id = q.pop(0)
+			if node_id in visited: continue
+			visited.add(node_id)
+			yield node_id
+			for child_id, child_weight in self.graph[node_id]:
+				q.append(child_id)
+
+	def bfs(self, start_node):
+		return self.search(start_node, search_type="bfs")
+
+	def dfs(self, start_node):
+		return self.search(start_node, search_type="dfs")
+
+	def __repr__(self):
+		dot = "digraph {\n"
+		for node_from, node_to, weight in self:
+			dot += "%s -> %s [weight=%s];\n" \
+				% (node_from, node_to, str(weight))
+		dot += "}"
+		return dot
+
